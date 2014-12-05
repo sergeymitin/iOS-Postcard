fcf0fee5a9211621ae7b57c861162a18d9530f70
Sergey Mitin <sergey.mitin@revosim.com>
2014-12-05 18:24:14 +0300

    My first revo of app

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -9,15 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var enterNameTextField: UITextField!
+    @IBOutlet weak var enterMessageTextField: UITextField!
+    @IBOutlet weak var mailButton: UIButton!
+    
+    
+    override func viewDidLoad() { super.viewDidLoad() }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+    override func didReceiveMemoryWarning() { super.didReceiveMemoryWarning() }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func sendMailButtonPressed(sender: UIButton) {
+        
+        messageLabel.hidden = false
+        messageLabel.text = enterMessageTextField.text
+        enterMessageTextField.text = ""
+        enterMessageTextField.resignFirstResponder()
+        messageLabel.textColor = UIColor.blueColor()
+        
+        mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+        
+        
     }
 
 
